53951cf5431d626018921affdb54c361dbe41645
nonocl <nono.disc94@gmail.com>
2026-05-02 17:40:09 +0200

    Add files via upload
    new

--- sw.js
+++ sw.js
@@ -1,12 +1,24 @@
-const CACHE = 'nonosworld-v1';
+const CACHE = 'systeme-n-v15';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-180.png',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './icons-pack/icon-manga-180.png',
+  './icons-pack/icon-manga-512.png',
+  './icons-pack/icon-kawaii-180.png',
+  './icons-pack/icon-kawaii-512.png',
+  './icons-pack/icon-aesthetic-180.png',
+  './icons-pack/icon-aesthetic-512.png',
+  './icons-pack/icon-plush-180.png',
+  './icons-pack/icon-plush-512.png'
 ];
+
+// Libs externes à cacher pour offline (lazy au premier accès)
+const EXT_CACHE = 'nonosworld-ext-libs-v1';
+const EXT_DOMAINS = ['cdn.jsdelivr.net', 'unpkg.com', 'cdnjs.cloudflare.com', 'tessdata.projectnaptha.com', 'fonts.googleapis.com', 'fonts.gstatic.com'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
@@ -17,20 +29,23 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== CACHE && k !== EXT_CACHE).map((k) => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  const isExt = EXT_DOMAINS.some(d => url.hostname.includes(d));
+  const cacheName = isExt ? EXT_CACHE : CACHE;
   e.respondWith(
     caches.match(e.request).then((cached) => {
       if (cached) return cached;
       return fetch(e.request).then((res) => {
-        if (res && res.status === 200 && res.type === 'basic') {
+        if (res && res.status === 200) {
           const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          caches.open(cacheName).then((c) => c.put(e.request, copy));
         }
         return res;
       }).catch(() => cached);
